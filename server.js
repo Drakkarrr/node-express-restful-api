6f012598dcbead645b07b0b7c2e35e188cb9212d
drakkarrr/resume <drakkar.alpha00@gmail.com>
2023-12-27 06:18:48 +0800

    added addons dependencies connected to db

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 8000;
+const MONGO_URI = 'mongodb+srv://admin:admin123@node-api.pmvypez.mongodb.net/';
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
@@ -10,5 +12,19 @@ app.listen(PORT, () => {
 // routes
 
 app.get('/', (req, res) => {
-  res.send('Hello World Node API');
+  res.send('Hello Node API');
 });
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Welcome to the Node API' });
+});
+
+// connect to mongodb
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
