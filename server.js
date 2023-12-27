2c3d869bd2e096f79d8c1eb429fddc2611e6a0a1
drakkarrr/resume <drakkar.alpha00@gmail.com>
2023-12-27 11:49:13 +0800

    implemented crud restful API

--- server.js
+++ server.js
@@ -52,9 +52,10 @@ app.put('/products/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const product = await Products.findByIdAndUpdate(id, req.body);
-    if (!product) res.status(404).json({ message: 'Product not found' });
     const updatedProduct = { ...product._doc, ...req.body };
-    res.status(200).json(updatedProduct);
+    !product
+      ? res.status(404).json({ message: 'Product not found' })
+      : res.status(200).json(updatedProduct);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
