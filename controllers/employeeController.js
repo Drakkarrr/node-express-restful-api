ad9b92c4ff1a22135099a1115ccdbef72419cfa3
drakkarrr/resume <drakkar.alpha00@gmail.com>
2023-12-28 06:42:48 +0800

    added middleware employee update

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -28,3 +28,17 @@ export const getEmployee = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const updateEmployee = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findByIdAndUpdate(id, req.body);
+    const updatedEmployee = { ...employee._doc, ...req.body };
+    !employee
+      ? res.status(404).json({ message: 'Employee not found' })
+      : res.status(200).json(updatedEmployee);
+    res.status(200).json(updatedEmployee);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
