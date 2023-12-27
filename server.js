f87c483ef088055992eafa9c26d7a7018eee62ef
drakkarrr/resume <drakkar.alpha00@gmail.com>
2023-12-27 11:36:41 +0800

    fet: update and return realtime  specific product data

--- server.js
+++ server.js
@@ -52,7 +52,8 @@ app.put('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Products.findByIdAndUpdate(id, req.body);
     if (!product) res.status(404).json({ message: 'Product not found' });
-    res.status(200).json(product);
+    const updatedProduct = { ...product._doc, ...req.body };
+    res.status(200).json(updatedProduct);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
