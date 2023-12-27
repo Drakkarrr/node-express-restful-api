75eed5b66790b88d09d97c80a2ed830284c4d123
drakkarrr/resume <drakkar.alpha00@gmail.com>
2023-12-27 10:13:18 +0800

    completed product model

--- models/productModel.js
+++ models/productModel.js
@@ -1,19 +1,38 @@
 import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: [true, 'Please enter product name'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please enter product description'],
+    },
+    quatity: {
+      type: Number,
+      required: [true, 'Please enter product quantity'],
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
   },
-  name: {
-    type: String,
-    required: [true, 'Please enter product name'],
-  },
-  description: {
-    type: String,
-    required: [true, 'Please enter product description'],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Product = mongoose.model('Product', productSchema);
 export default Product;
