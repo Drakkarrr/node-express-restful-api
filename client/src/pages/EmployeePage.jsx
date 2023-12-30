a04d1dc770c506a76adecc06408608ce8dbd1fe0
drakkarrr/resume <drakkar.alpha00@gmail.com>
2023-12-30 11:00:28 +0800

    course complete

--- client/src/pages/EmployeePage.jsx
+++ client/src/pages/EmployeePage.jsx
@@ -7,6 +7,7 @@ const EmployeesPage = () => {
     const response = await fetch('http://localhost:8000/api/employees');
     const data = await response.json();
     setEmployees(data);
+    console.log(data);
   };
   return (
     <div>
@@ -18,13 +19,15 @@ const EmployeesPage = () => {
           <table key={employee.name}>
             <tr>
               <th>Employee Name</th>
-              <th>Address</th>
+              <th>Email</th>
               <th>Phone</th>
+              <th>Postition</th>
             </tr>
             <tr>
               <td>{employee.name}</td>
-              <td>{employee.address}</td>
+              <td>{employee.email}</td>
               <td>{employee.phone}</td>
+              <td>{employee.position}</td>
             </tr>
           </table>
         ))}
