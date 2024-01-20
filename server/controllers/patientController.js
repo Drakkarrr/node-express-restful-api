5290fbbfd9b20a05608fc40e63905cc2918ba0b1
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-21 01:45:46 +0800

    fet: create patient and return data with error handling

--- server/controllers/patientController.js
+++ server/controllers/patientController.js
@@ -1,5 +1,14 @@
 import Patients from '../models/patientModel.js';
 
+export const createPatient = async (req, res) => {
+  try {
+    const patient = await Patients.create(req.body);
+    res.status(200).json(patient);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 export const getPatients = async (req, res) => {
   try {
     const patients = await Patients.find();
