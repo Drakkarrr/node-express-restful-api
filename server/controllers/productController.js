831d1d4de3fcdadf773724eb5ab3d4faf6e6680b
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-21 01:09:16 +0800

    remove console

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -14,7 +14,6 @@ export const createProduct = async (req, res) => {
     const product = await Products.create(req.body);
     res.status(200).json(product);
   } catch (error) {
-    console.log(error);
     res.status(500).json({ message: error.message });
   }
 };
