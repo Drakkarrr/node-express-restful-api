24c45a350320db63d6d8880e88b0487ac7ff7e98
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-21 01:36:27 +0800

    completed fetch all patients

--- server/controllers/patientController.js
+++ server/controllers/patientController.js
@@ -1,5 +1,17 @@
+import Patients from '../models/patientModel.js';
+
 export const getPatients = async (req, res) => {
   try {
+    const patients = await Patients.find();
+    res.status(200).json(patients);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getPatient = async (req, res) => {
+  try {
+    const { id } = req.params;
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
