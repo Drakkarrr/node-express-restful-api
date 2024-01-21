a3dee218d9f56097754deba98f36ebf5454bdcff
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-22 06:15:01 +0800

    fet: added form reminders submit and search feature display each reminder list

---
+++ client/src/components/ReminderList.tsx
@@ -0,0 +1,25 @@
+import { Reminder } from '../types/reminder';
+
+interface ReminderListProps {
+  items: Reminder[];
+  onDeleteReminder: (id: number) => void;
+}
+
+const ReminderList = ({ items, onDeleteReminder }: ReminderListProps) => {
+  return (
+    <ul className='list-group'>
+      {items.map((item) => (
+        <li className='list-group-item' key={item.id}>
+          {item.title}
+          <button
+            onClick={() => onDeleteReminder(item.id)}
+            className='btn btn-outline-danger mx-5 rounded-pill'>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ReminderList;
