a60372d611c5a385ee210f953f6aff325e820b68
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-23 06:00:06 +0800

    added basic useFetch util

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,36 @@
+import { useEffect, useReducer, useRef } from 'react';
+
+export function useFetch(url, options) {
+  const cache = useRef({});
+
+  const cancelRequest = useRef(false);
+
+  const initialState = {
+    error: undefined,
+    data: undefined,
+  };
+
+  useEffect(() => {
+    if (!url) return;
+
+    cancelRequest.current = false;
+
+    const fetchData = async () => {
+      dispatch({ type: 'loading' });
+
+      if (cache.current[url]) {
+        dispatch({ type: 'fetched', payload: cache.current[url] });
+        return;
+      }
+    };
+
+    void fetchData();
+
+    return () => {
+      cancelRequest.current = true;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [url]);
+
+  return state;
+}
