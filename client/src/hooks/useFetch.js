3a0594ad301c82e255b1d3e6273310ab65d1b671
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-23 06:00:54 +0800

    added fetch reducer status state

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -10,6 +10,19 @@ export function useFetch(url, options) {
     data: undefined,
   };
 
+  const fetchReducer = (state, action) => {
+    switch (action.type) {
+      case 'loading':
+        return { ...initialState };
+      case 'fetched':
+        return { ...initialState, data: action.payload };
+      case 'error':
+        return { ...initialState, error: action.payload };
+      default:
+        return state;
+    }
+  };
+
   const [state, dispatch] = useReducer(fetchReducer, initialState);
 
   useEffect(() => {
